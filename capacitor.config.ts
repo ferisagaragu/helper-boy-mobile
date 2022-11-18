631c6b27a127195388b6cee128f995e42d374f9d
ferisagaragu <fernaisagaragu@gmail.com>
2022-11-18 02:15:09 -0600

    I change the aplication name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.pechblenda.helperboyapp',
+  appId: 'org.pechblenda.helperboymobile',
   appName: 'Pequeño ayudador',
   webDir: 'www',
   bundledWebRuntime: false
