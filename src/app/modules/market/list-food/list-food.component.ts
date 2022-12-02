f005509e0820c7a4bfed05af707c2a7953cb15e3
ferisagaragu <fernaisagaragu@gmail.com>
2022-12-02 13:03:58 -0600

    I exclude files and add exclude works

--- src/app/modules/market/list-food/list-food.component.ts
+++ src/app/modules/market/list-food/list-food.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { FoodModel } from '../../../core/model/food.model';
 
 @Component({
